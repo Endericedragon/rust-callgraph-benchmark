8eda98a1dd62bd23da5a8bd4d7b8fe8f4a7d3ac9
Endericedragon Fu <fuze1007@163.com>
2025-03-14 21:13:06 +0800

    dead_code_remover.py已经可以识别死代码了

---
+++ src/main/dead_code_remover.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+
+# from collections import defaultdict
+# import json
+
+# with open("om.json", "r", encoding="utf-8") as f:
+#     om = json.load(f)
+
+# dir2func: dict[Path, list[dict]] = defaultdict(list)
+
+# for func_metadata in om["func_metadata"]:
+#     if ".rustup/toolchains" in func_metadata["define_path"]:
+#         continue
+#     dir2func[Path(func_metadata["define_path"])].append(func_metadata)
+
+# for k in dir2func.keys():
+#     dir2func[k] = sorted(dir2func[k], key=lambda x: int(x["line_num"]))
+
+# for k, v in dir2func.items():
+#     print(k)
+#     with open(k, "r", encoding="utf-8") as f:
+#         content = f.readlines()
+#     for each in v:
+#         print(f"\t{each['def_id']}, line_num = {each['line_num']}")
+#         content[each["line_num"]] = content[each["line_num"]].strip() + f"/* used */\n"
+#     with open(k, "w", encoding="utf-8") as f:
+#         f.writelines(content)
+from re import search
+import json
+from collections import defaultdict
+from bisect import bisect_left
+
+root = Path(".")
+
+
+def find_rust_func_span(lines: list[str], lo: int) -> tuple[int, int]:
+    hi: int = lo
+    curly_brackets: int = 0
+    while curly_brackets == 0:
+        if lines[hi].lstrip() == "//":
+            hi += 1
+            continue
+        curly_brackets += lines[hi].count("{")
+        if curly_brackets > 0:
+            curly_brackets -= lines[hi].count("}")
+            hi += 1
+            break  # 函数签名后的函数体开始了
+        elif lines[hi].rstrip().endswith(";"):
+            # trait方法
+            return (lo, hi + 1)
+        else:
+            curly_brackets -= lines[hi].count("}")
+            hi += 1
+    while curly_brackets > 0:
+        curly_brackets += lines[hi].count("{") - lines[hi].count("}")
+        hi += 1
+    return (lo, hi)
+
+
+def traverse(path: Path):
+    for child in path.iterdir():
+        if child.is_dir():
+            traverse(child)
+        elif child.suffix == ".rs":
+            with open(child, "r", encoding="utf-8") as f:
+                content = f.readlines()
+            for line_num, line in enumerate(content):
+                line = line.strip()
+                if line.startswith("//"):
+                    continue
+                if search(r"fn [a-zA-Z_][a-zA-Z_\d]+\(", line) is not None:
+                    print("File:", child)
+                    span = find_rust_func_span(content, line_num)
+                    for i in range(span[0], span[1]):
+                        print(f"{i:04d}|{content[i].rstrip()}")
+            del content
+
+
+# traverse(root)
+
+with open("om.json", "r", encoding="utf-8") as f:
+    om = json.load(f)
+
+dir_tree: defaultdict[Path, list[int]] = defaultdict(list)
+
+for each in om["func_metadata"]:
+    if ".rustup/toolchains" in each["define_path"]:
+        continue
+    dir_tree[Path(each["define_path"])].append(each["line_num"])
+
+del om
+
+for source_code_path, func_infos in dir_tree.items():
+    func_infos.sort()
+    with open(source_code_path, "r", encoding="utf-8") as f:
+        content = f.readlines()
+    for line_num, line in enumerate(content):
+        if search(r"fn [a-zA-Z_][a-zA-Z_\d]+\(", line) is not None:
+            span = find_rust_func_span(content, line_num)
+            if func_infos[bisect_left(func_infos, line_num)] != span[0]:
+                print(f"Dead code found in {source_code_path}!")
+                for i in range(span[0], span[1]):
+                    print(f"{i:04d}|{content[i].rstrip()}")
